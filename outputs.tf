7f7c61a4192686bf194e16787aa3c85259263340
Ronaldo <ronaldolauture@gmail.com>
2020-05-30 17:10:53 -0400

    added output to output.tf file

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "public_subnet_id" {
+  value = var.public_subnet_id
+}
+
+output "private_subnet_id" {
+  value = var.public_subnet_id
+}
+
+output "security_group_id" {
+  value = var.security_group_id
+}
+
+output "instance_id" {
+  value = google_compute_instance.this[0].self_link
+}
+
+output "public_ip" {
+  value = google_compute_address.this[0].address
+}
+
+output "private_ip" {
+  value = google_compute_instance.this[0].network_interface.1.network_ip
+}
